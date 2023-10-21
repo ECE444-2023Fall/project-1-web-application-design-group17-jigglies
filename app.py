48178367eea2d90549e0080c5b685d9c7ecc7a2a
Jason Wang <38818672+jasonhy-wang@users.noreply.github.com>
2023-10-21 15:40:20 -0400

    Improved authentication (#9)
    - added security check to ensure only uoft emails can be used to sign up, and enabled login with email as well
    
    - added validation to make sure home page can only be accessed once logged in

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user,login_required 
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -12,6 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'signup'
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,23 +22,33 @@ class User(UserMixin, db.Model):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
+
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('username')
+        user_identifier = request.form.get('user_identifier')
         password = request.form.get('password')
-        user = User.query.filter_by(username=username).first()
+        
+        # First, try to get the user by username
+        user = User.query.filter_by(username=user_identifier).first()
+        
+        # If not found by username, try email
+        if not user:
+            user = User.query.filter_by(email=user_identifier).first()
+
         if user and check_password_hash(user.password, password):  
             login_user(user)
             return redirect(url_for('index'))
         else:
-            flash('Login Unsuccessful. Check username and password', 'danger')
+            flash('Login Unsuccessful. Check your details and try again.', 'danger')
+
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -46,6 +57,11 @@ def signup():
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
+
+        # Add this block to validate the email domain
+        if not email.endswith('utoronto.ca'):
+            flash('Please sign up with a uoft email.', 'danger')
+            return render_template('signup.html')
 
         user_by_username = User.query.filter_by(username=username).first()
         user_by_email = User.query.filter_by(email=email).first()
@@ -62,7 +78,7 @@ def signup():
         new_user = User(username=username, email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        flash('Registration successful. Please login.', 'success')  # A message to inform the user that the registration was successful
+        flash('Registration successful. Please login.', 'success')
         return redirect(url_for('login'))
 
     return render_template('signup.html')
