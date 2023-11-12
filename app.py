739370bb916cc93eae196b9c794c14453ab36e25
Jason Wang <jason98wang@hotmail.com>
2023-11-11 23:31:43 -0500

    changed password to text to handler longer hashes

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)  # New email field
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.Text, nullable=False)
     comments = db.relationship("Comment", backref="user", passive_deletes=True)
     likes = db.relationship("Like", backref="user", passive_deletes=True)
     rsvps = db.relationship("Rsvp", backref="user", passive_deletes=True)
