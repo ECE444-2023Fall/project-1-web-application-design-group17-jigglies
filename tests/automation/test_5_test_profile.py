5c1469455bea2c0fa9e4ca6c6b8d812c72a5ab0e
rawwash <yousef.alrawwash@mail.utoronto.ca>
2023-11-13 19:53:09 -0500

    fixing IDs

--- tests/automation/test_5_test_profile.py
+++ tests/automation/test_5_test_profile.py
@@ -26,24 +26,24 @@ def test_5_test_profile(browser, client):
 
     # Update the username
     new_username = 'newusername'
-    username_field = browser.find_element(By.NAME, 'username')
-    username_field.clear()
-    username_field.send_keys(new_username)
+    username_input = browser.find_element(By.ID, 'username')
+    username_input.clear()
+    username_input.send_keys(new_username)
 
     # Update the password
     new_password = 'newpassword123'
-    password_field = browser.find_element(By.NAME, 'password')
-    password_field.clear()
-    password_field.send_keys(new_password)
+    password_input = browser.find_element(By.ID, 'password')
+    password_input.clear()
+    password_input.send_keys(new_password)
 
     # Update the bio
     new_bio = 'Updated bio text'
-    bio_input = browser.find_element(By.NAME, 'bio')
+    bio_input = browser.find_element(By.ID, 'bio')
     bio_input.clear()
     bio_input.send_keys(new_bio)
 
     # Submit the form
-    save_button = browser.find_element(By.XPATH, "//button[contains(text(), 'Save Changes')]")
+    save_button = browser.find_element(By.XPATH, "//input[@value='Update Profile']")
     save_button.click()
 
     # Log out
@@ -61,11 +61,11 @@ def test_5_test_profile(browser, client):
     submit_button.click()
 
     # Verify the login was successful with new credentials
-    assert browser.current_url == 'http://web-app:5000/', "Login was unsuccessful with new credentials"
+    assert browser.current_url == 'http://web-app:5000/', f'Login was unsuccessful, url was expected to be http://web-app:5000/ but is actually {browser.current_url}'
 
     # Navigate to the profile page to verify bio update
-    profile_link = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, 'Profile')))
-    profile_link.click()
+    profile_link = "http://web-app:5000/edit-profile"
+    browser.get(edit_profile_url)
 
     # Check if the bio has been updated
     updated_bio = browser.find_element(By.ID, 'user-bio').text
