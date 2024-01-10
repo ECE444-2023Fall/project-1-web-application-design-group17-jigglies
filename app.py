0f2091bdd659ec72d4e984eb5c19805de7482847
John Lee <jhlee90741@gmail.com>
2024-01-10 18:07:49 -0500

    completed untested backend code

--- app.py
+++ app.py
@@ -150,13 +150,14 @@ def index():
     events_today = Event.query.filter_by(date=today).all()
     user_events = Event.query.filter_by(created_by=current_user.id).all()
     
+    
     top_upcoming_rsvps = Event.query \
         .join(Rsvp, Event.id == Rsvp.event_id) \
         .filter(Rsvp.author == current_user.id, Event.date >= today) \
         .order_by(Event.date.asc()) \
         .limit(5) \
         .all()
-        
+
     top_liked_events = Event.query \
         .join(Like, isouter=True) \
         .group_by(Event.id) \
@@ -165,7 +166,22 @@ def index():
         .all()
     
     events = Event.query.all()
-    return render_template('index.html', events = events, events_today=events_today, user_events=user_events, top_upcoming_rsvps=top_upcoming_rsvps, top_liked_events=top_liked_events)
+
+    if session['search'] is not None:
+        recommended_events = Event.query.join(User, Event.created_by == User.id).filter( \
+         (Event.event_name.ilike(f'%{session["search"]}%')) | \
+         (User.username.ilike(f'%{session["search"]}%')))
+    else:
+        recommended_events = None
+        # if(query):
+    #     results = Event.query.join(User, Event.created_by == User.id).filter(
+    #     (Event.event_name.ilike(f'%{query}%')) |
+    #     (User.username.ilike(f'%{query}%'))
+    # ).all()
+    #     description = "Search Results: " + query
+    #     session['search'] = query #Makes the latest search query associate with the users cookies
+
+    return render_template('index.html', events = events, events_today=events_today, user_events=user_events, top_upcoming_rsvps=top_upcoming_rsvps, top_liked_events=top_liked_events, recommended_events=recommended_events)
 
 
 @app.route('/login', methods=['GET', 'POST'])
